bb99cfd30c9f5045667e3ce514853ad2d6aec880
osaga-fu <151243613+osaga-fu@users.noreply.github.com>
2024-02-15 10:01:20 +0100

    refactored tarjeta test

--- src/components/Tarjetita.jsx
+++ src/components/Tarjetita.jsx
@@ -1,11 +1,9 @@
-import { modifySelectionPerMouseMove } from "@testing-library/user-event/dist/types/event";
-
-export default function Tarjetita({title, year}) {
-
+export default function Tarjetita({ title, year }) {
   return (
     <div>
-        <h1>`{title} ({year})`</h1>
-        </div>
-  )
+      <h1>
+        `${title} (${year})`
+      </h1>
+    </div>
+  );
 }
-
